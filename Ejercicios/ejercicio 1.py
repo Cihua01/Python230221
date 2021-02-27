299036140897a7220d869aabab4ff7e7828d5a9a
Pili <tarinfuv@gmail.com>
2021-02-26 18:09:24 -0600

    ej1

---
+++ Ejercicios/ejercicio 1.py
@@ -0,0 +1,17 @@
+#declarar las variables
+print ("escribe numero1: ")
+calificacion1= int (input ())
+
+
+calificacion2= int (input ("escribe numero2: "))
+
+print ("escribe numero3: ")
+calificacion3= int (input ())
+
+
+#operaciones
+suma=calificacion1 + calificacion2 + calificacion3
+promedio=(suma/3)
+
+
+print(f"el promedio es igual a={promedio} ")
